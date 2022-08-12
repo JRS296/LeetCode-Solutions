7116bbd141361a219c3643d20ae36cf8822bf717
JRS296 <jonathansamuel296@gmail.com>
2022-08-12 21:45:46 +0530

    Commit #8 - Modified 2 file(s) - 12.08.2022 @21:45

--- Arrays & Strings/Pascal's Traingle.java
+++ Arrays & Strings/Pascal's Traingle.java
@@ -1,3 +1,26 @@
-public class Pascal's Traingle {
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>(numRows);
+        for(int i=1; i<=numRows; i++)
+        {
+            ArrayList<Integer> a = new ArrayList<Integer>(Collections.nCopies(i, 1));
+            ret.add(a);
+            if(i>1)
+            {
+                for(int j=0; j<i-1; j++)
+                {
+                    System.out.println(ret.get(i-1)+" "+j);
+                    int x = ret.get(i-1).get(j) + ret.get(i-1).get(j+1);
+                    System.out.println(x);
+                    ret.get(i-1).set(j+1,x);
+                }
+            }
+        }
+        
+        return ret;
+    }
 }
