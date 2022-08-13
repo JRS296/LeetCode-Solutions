4aa12df85746e7dc65ed27fbd903901f4ce9b473
JRS296 <jonathansamuel296@gmail.com>
2022-08-13 22:24:50 +0530

    Commit #9 - Modified 2 file(s) - 13.08.2022 @22:24

--- Arrays & Strings/Pascal's Traingle.java
+++ Arrays & Strings/Pascal's Traingle.java
@@ -13,14 +13,52 @@ class Solution {
             {
                 for(int j=0; j<i-1; j++)
                 {
-                    System.out.println(ret.get(i-1)+" "+j);
-                    int x = ret.get(i-1).get(j) + ret.get(i-1).get(j+1);
-                    System.out.println(x);
-                    ret.get(i-1).set(j+1,x);
+                    //System.out.println(ret.get(i-1)+" "+j);
+                    if(j>0 && j<i-1)
+                    {
+                        int x = ret.get(i-2).get(j) + ret.get(i-2).get(j-1);
+                        //System.out.println(x +" "+ret.get(i-1));
+                        ret.get(i-1).set(j, x);
+                    }
+                    //ret.get(i-1).set(j+1,x);
                 }
             }
         }
-        
+        //System.out.println(ret);
         return ret;
     }
 }
+
+/*
+Runtime: 1 ms (very Good)
+Memory Usage: 42.5 MB (poor but understandable)
+ */
+
+class Solution2 {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i = 0 ; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            
+            for(int j = 0; j < i + 1 ; j++) {
+                if(j == 0 || j == i) {
+                    list.add(1);
+                }
+                else {
+                    int a = result.get(i - 1).get(j - 1);
+                    int b = result.get(i - 1).get(j);
+                    list.add(a + b);
+                }
+            }
+            result.add(list);
+         }
+        return result;
+      }
+}
+
+ /*
+Runtime: 1 ms (very good)
+Memory Usage: 41.4 MB (tops 705, pretty efficient)
+  */
