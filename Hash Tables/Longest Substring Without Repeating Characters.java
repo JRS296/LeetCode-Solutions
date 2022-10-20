925656b7ad504e3a17828b98e68d9eb7a491389f
JRS296 <jonathansamuel296@gmail.com>
2022-10-20 23:14:46 +0530

    Commit #43 - Modified 1 file(s) - 20.10.2022 @23:14

--- Hash Tables/Longest Substring Without Repeating Characters.java
+++ Hash Tables/Longest Substring Without Repeating Characters.java
@@ -33,4 +33,46 @@ class Solution {
 317 / 987 test cases passed.
  */
 
-//Attempt #2 - 
+//Attempt #2 - 10mins
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> x = new HashSet<Character>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int length = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(x.contains(s.charAt(i)))
+            {
+                ans.add(length);
+                length = 0;
+                x.clear();
+                x.add(s.charAt(i));
+            }
+            else
+            {
+                x.add(s.charAt(i));
+                length++;
+            }
+            //System.out.println(x);
+            ans.add(x.size());
+        }
+        ans.add(length);
+        ans.add(x.size());
+        //System.out.println(ans);
+        int great = -1;
+        for(int i=0; i<ans.size(); i++)
+        {
+            if(ans.get(i)>great)
+            {
+                great = ans.get(i);
+            }
+        }
+        
+        return great;
+    }
+}
+
+/*
+407 / 987 test cases passed.
+ */
