fda7312c4cd04298fc9bbd329cfd2ace26dc5a57
JRS296 <jonathansamuel296@gmail.com>
2022-11-02 00:06:28 +0530

    Commit #49 - Modified 1 file(s) - 02.11.2022 @00:05

---
+++ Binary Search/Search in Rotated Sorted Array.java
@@ -0,0 +1,69 @@
+//Attempt #1 - 5mins
+//Link: https://leetcode.com/submissions/detail/834860564/?from=explore&item_id=952
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (target == nums[i])
+                return i;
+        }
+        return -1;
+
+    }
+}
+
+/*
+ * 195 / 195 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ * Memory Usage: 42 MB
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+// Solution using Binary Search
+class Solution {
+    public int search(int[] nums, int target) {
+        int minIdx = findMinIdx(nums);
+        if (target == nums[minIdx])
+            return minIdx;
+        int m = nums.length;
+        int start = (target <= nums[m - 1]) ? minIdx : 0;
+        int end = (target > nums[m - 1]) ? minIdx : m - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (target > nums[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+
+    public int findMinIdx(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end])
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return start;
+    }
+}
+
+/*
+195 / 195 test cases passed.
+Status: Accepted
+Runtime: 1 ms
+Memory Usage: 42.1 MB
+ * 
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ */
